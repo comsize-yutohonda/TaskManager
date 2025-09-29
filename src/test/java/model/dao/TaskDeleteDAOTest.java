5afdc41602304d908d4009cbdb2791c224f49ac2
comsize-yutohonda <yuto.honda@comsize.com>
2025-09-29 15:14:42 +0900

    カバレッジレポート完成

--- src/test/java/model/dao/TaskDeleteDAOTest.java
+++ src/test/java/model/dao/TaskDeleteDAOTest.java
@@ -2,13 +2,36 @@ package model.dao;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.sql.SQLException;
+
 import org.junit.jupiter.api.Test;
 
 class TaskDeleteDAOTest {
 
 	@Test
-	void testDeleteTask() {
-		fail("まだ実装されていません");
+	void testDeleteTask_成功() {
+		TaskDeleteDAO dao = new TaskDeleteDAO();
+		try {
+			assertTrue(dao.deleteTask(3));
+			
+		} catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	void testDeleteTask_失敗() {
+		TaskDeleteDAO dao = new TaskDeleteDAO();
+		try {
+			assertFalse(dao.deleteTask(0));
+			
+		} catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
